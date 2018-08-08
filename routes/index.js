554ebd86d137b1a4be246033378404aa1b035b5d
François Dubois <francoisdubois@hotmail.com>
2018-08-08 18:35:32 +0200

    sdkjdlsdlsl

--- routes/index.js
+++ routes/index.js
@@ -107,7 +107,7 @@ router.delete('/delete', function(req,res,next){
 //Route pour afficher les taches crées par l'utilisateur
 router.get('/tachesCrees', function(req, res, next) {
   taskModel.find(function (erreur, resultat) {
-
+console.log('toto');
 
     res.json(resultat);
    });
